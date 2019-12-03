ecbd69d8e79fb3df28f847a47650e8586f14be7f
Cezary Zachwieja <cezary.zachwieja@gmail.com>
2019-12-04 00:57:35 +0100

    day1 part2

--- src/day1/Day1.java
+++ src/day1/Day1.java
@@ -8,21 +8,27 @@ public class Day1 {
     public static void main(String[] args) throws FileNotFoundException {
 
         double sum = 0.0;
+        double sumOfAllMpdules = 0.0;
 
         File file = new File("src/day1/input.txt");
         Scanner scanner = new Scanner(file);
 
         while (scanner.hasNext()) {
+            double sumPerModule = 0.0;
             double mass = scanner.nextInt();
             double fuel = Math.floor(mass / 3) - 2;
             sum += fuel;
+            while (fuel > 0) {
+                sumPerModule += fuel;
+                fuel = Math.floor(fuel / 3) - 2;
+            }
+            sumOfAllMpdules += sumPerModule;
         }
 
         System.out.println("--- Day 1: The Tyranny of the Rocket Equation ---\n");
         System.out.println("The sum of the fuel requirements for all of the modules on my spacecraft is " + (int) (sum));
+        System.out.println("The sum of the fuel requirements for all of the modules on my spacecraft when also taking into account the mass of the added fuel is " + (int) (sumOfAllMpdules));
+
     }
 
 }
-
-
-
