856c39ec6ee7ab3c4bcd1e6704aeb13a79dcc7c3
Cezary Zachwieja <cezary.zachwieja@gmail.com>
2020-12-11 21:34:25 +0100

    Day6 part2

--- 2020/day6/Day6CustomCustoms.java
+++ 2020/day6/Day6CustomCustoms.java
@@ -13,19 +13,31 @@ public class Day6CustomCustoms {
         List<String> inputList = getListFromInput(pathToInputFileDay6);
 
         int counterAnyoneYes = 0;
+        int counterEveryoneYes = 0;
+        int lineCounter = 0;
+        
         StringBuilder sb = new StringBuilder();
 
         for (String s : inputList) {
             sb.append(s);
             if (s.isEmpty()) {
                 counterAnyoneYes += countUniqueCharsInStringBuilder(sb);
+                counterEveryoneYes += countEveryOneSaidYes(sb, lineCounter);
                 sb = new StringBuilder();
+                lineCounter = 0;
+            } else {
+                lineCounter++;
             }
         }
 
         // last sb
+        //part 1
         counterAnyoneYes += countUniqueCharsInStringBuilder(sb);
         System.out.println(counterAnyoneYes);
+
+        // part 2
+        counterEveryoneYes += countEveryOneSaidYes(sb, lineCounter);
+        System.out.println(counterEveryoneYes);
     }
 
     private static List<String> getListFromInput(String pathToInputFileDay6) {
@@ -44,5 +56,25 @@ public class Day6CustomCustoms {
             characterSet.add(sb.charAt(i));
         }
         return characterSet.size();
+    }
+
+    private static int countEveryOneSaidYes(StringBuilder sb, int lineCounter) {
+
+        int countEveryOneSaidYes = 0;
+        List<Character> characterList = new ArrayList<>();
+        Set<Character> characterSet = new HashSet<>();
+
+        for (int i = 0; i < sb.length(); i++) {
+            char charAt = sb.charAt(i);
+            characterList.add(charAt);
+            characterSet.add(charAt);
+        }
+
+        for (Character character : characterSet) {
+            if (Collections.frequency(characterList, character) == lineCounter) {
+                countEveryOneSaidYes++;
+            }
+        }
+        return countEveryOneSaidYes;
     }
 }
