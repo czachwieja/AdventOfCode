50b8b2c54e5ddca6d24076e6e909f47858ac7247
Cezary Zachwieja <cezary.zachwieja@gmail.com>
2020-12-17 15:13:49 +0100

    getListFromInputFile by given package name added in Utilities

---
+++ 2020/day8/HandheldHalting.java
@@ -0,0 +1,16 @@
+package day8;
+
+import java.util.List;
+
+import static util.Utilities.getListFromInputFile;
+
+public class HandheldHalting {
+
+    public static void main(String[] args) {
+
+        List<String> inputList = getListFromInputFile(HandheldHalting.class.getPackageName());
+
+
+    }
+
+}
