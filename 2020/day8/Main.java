5a2a58ee39a6d52d424b07b571dd77516747398a
Cezary Zachwieja <cezary.zachwieja@gmail.com>
2020-12-28 22:30:14 +0100

    Day8 part2

--- 2020/day8/Main.java
+++ 2020/day8/Main.java
@@ -10,18 +10,46 @@ public class Main {
         HandheldHalting handheldHalting = new HandheldHalting();
         List<String> inputList = handheldHalting.inputList;
 
-        int accumulator = getAccumulator(inputList);
+        int accumulator = getAccumulator(inputList, 1);
         System.out.println("Loop starts at value = " + accumulator + " in the accumulator.");
+
+        accumulator = getAccumulatorWhenProgramTerminates(inputList);
+        System.out.println("Program terminates at value of the accumulator = " + accumulator + ".");
 
     }
 
-    private static int getAccumulator(List<String> inputList) {
+    private static int getAccumulatorWhenProgramTerminates(List<String> inputList) {
+
+        int accumulator = 0;
+
+        for (int i = 0; i < inputList.size(); i++) {
+            String instruction = inputList.get(i);
+
+            if (instruction.contains("jmp")) {
+                inputList.set(i, instruction.replace("jmp", "nop"));
+            } else if (instruction.contains("nop")) {
+                inputList.set(i, instruction.replace("nop", "jmp"));
+            }
+
+            accumulator = getAccumulator(inputList, 2);
+            if (accumulator != -1) {
+                break;
+            }
+            inputList.set(i, instruction);
+        }
+        return accumulator;
+    }
+
+    private static int getAccumulator(List<String> inputList, int part) {
 
         int accumulator = 0;
         List<Integer> iterationsList = new ArrayList<>();
 
         for (int i = 0; i < inputList.size(); i++) {
             if (iterationsList.contains(i)) {
+                if (part == 2 && i < inputList.size() - 1) {
+                    accumulator = -1;
+                }
                 break;
             }
             iterationsList.add(i);
